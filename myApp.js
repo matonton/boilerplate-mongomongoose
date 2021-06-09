1953e8bf58e3cf74e09542a81f09708de65c9d93
matonton <30802662+matonton@users.noreply.github.com>
2021-06-09 05:39:50 +0000

    added query

--- myApp.js
+++ myApp.js
@@ -3,26 +3,61 @@ const mongoose = require('mongoose');
 
 mongoose.connect('mongodb+srv://hoopla:XxfQnridOJlUHvkC@cluster0.jk4rw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 
-const db = mongoose.connection;
-
+/* const db = mongoose.connection;
 // console.log(db);
 db.on('error', () => console.error('connection error'));
 db.once('open', function() {
   console.log("welcome to your db");
+});*/
+
+// build Schema
+const { Schema } = mongoose;
+var personSchema = new mongoose.Schema({
+  name: String,
+  age: { type: Number, required: true },
+  favoriteFoods: [String]
 });
 
-let Person;
+// build model
+var Person = mongoose.model('Person', personSchema);
+/*
+follow this pattern for asynchronous operations in Node convention
+const someFunc = function(done) {
+  //... do something
+  if (error) return done(error);
+  done(null, result);
+};*/
 
-const createAndSavePerson = (done) => {
-  done(null /*, data*/);
+var createAndSavePerson = function(done) {
+  // build document
+  var silence = new Person({ name: 'Silence', age: 12, favoriteFoods: ['rags', 'weeds'] });
+  
+  console.log(silence);
+  
+  silence.save(function(err, data) {
+    if (err) return console.error(err);
+    done(null, data)
+    });
 };
 
+var arrayOfPeople = [
+  {name: "Frankie", age: 74, favoriteFoods: ["Del Taco"]},
+  {name: "Sol", age: 76, favoriteFoods: ["roast chicken"]},
+  {name: "Robert", age: 78, favoriteFoods: ["wine"]}
+];
+
 const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
+  Person.create(arrayOfPeople, function(err, people) {
+    if (err) return console.log(err);
+    done(null, people);
+  });
 };
 
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+  Person.find({ 'name': personName}, function(err, result) {
+    if (err) return console.log(err);
+    done(null, result);
+  })
 };
 
 const findOneByFood = (food, done) => {
